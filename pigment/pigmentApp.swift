477b880046d30ce905877bc7bac56baedd8a0b27
starburst3190 <jackuang0008@gmail.com>
2026-09-19 00:14:07 +0800

    Initial Commit

---
+++ pigment/pigmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  pigmentApp.swift
+//  pigment
+//
+//  Created by Jack on 2026/9/19.
+//
+
+import SwiftUI
+
+@main
+struct pigmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
